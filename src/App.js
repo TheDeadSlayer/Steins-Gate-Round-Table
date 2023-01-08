36338d49247ada813ad6bdb52078310e3b5fe2ad
MKom-Sama <mkomsany3@yahoo.com>
2023-01-08 23:54:57 +0200

    protect routes

--- src/App.js
+++ src/App.js
@@ -5,20 +5,34 @@ import Create  from './Create/Create';
 import Home  from './Homepage/Homepage';
 import Navbar from './Navbar/Navbar'
 import Chatrooms from './Chatrooms/Chatrooms'
+import { AuthenticatedUser } from './context/authState';
+import { useContext } from 'react';
 
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { BrowserRouter, Route, Routes, Redirect } from "react-router-dom"
 
 function App() {
+  const { isAuthenticated } = useContext(AuthenticatedUser);
+
   return (
     <div className="App-page">
 
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path="/Login" element={ <Login/>} />
-          <Route path="/Create" element={ <Create/>} />
-          <Route path="/Home" element={ <Home/>} />
-          <Route path="/Chatrooms" element={ <Chatrooms/>} />
+          {isAuthenticated ? 
+            <>
+              {/* protected routes */}
+              <Route path='/' element={<Home/>}/>
+              <Route path="/Login" element={ <Login/>} />
+              <Route path="/Create" element={ <Create/>} />
+              <Route path="/Home" element={ <Home/>} />
+              <Route path="/Chatrooms" element={ <Chatrooms/>} />
+            </>
+            :
+            <>
+              <Route path="/Create" element={<Create />} /> 
+              <Route path="*" element={ <Login/>} />
+            </>
+          }
         </Routes>
       </BrowserRouter>
 
--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { AuthenticatedUserProvider } from './context/authState';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-
-  <App />
-
+  <AuthenticatedUserProvider>
+    <App />
+  </AuthenticatedUserProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
