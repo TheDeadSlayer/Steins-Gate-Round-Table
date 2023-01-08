1fe321d1764ef3fd8a033c8d12b6be77cb4c84ec
MKom-Sama <mkomsany3@yahoo.com>
2023-01-09 00:01:29 +0200

    deploy structure

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+const PORT = process.env.PORT || 8060;
+
+const server = app.listen(PORT, console.log(`Listening to PORT:${PORT}`));
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "client/build")));
+  
+    app.get("/*", (req, res) => {
+      const index = path.join(__dirname, "client", "build", "index.html");
+      res.sendFile(index);
+    });
+}
